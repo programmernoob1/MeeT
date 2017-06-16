4604396648ad05450c8a5e6d54ef54496c23070c
programmernoob1 <sgopalan58@gmail.com>
2017-06-16 23:05:57 +0530

    Added proc file

--- ping/urls.py
+++ ping/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from authentication import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^auth/',include('authentication.urls')),
     url(r'^home/',include('home.urls')),
     url(r'^profile/',include('userprofile.urls')),
+    url(r'^$',views.login),	
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
